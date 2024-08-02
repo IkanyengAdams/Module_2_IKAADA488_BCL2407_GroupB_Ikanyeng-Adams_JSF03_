df0cc15bd07b6981cbba79651a8d6faa6f3a2b0d
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-08-02 15:41:29 +0200

    Added product details and rating to Productlist component

--- src/components/Productlist.vue
+++ src/components/Productlist.vue
@@ -3,7 +3,15 @@
   <div v-else class="products-container">
     <div v-for="product in products" :key="product.id" class="product-card">
       <img :src="product.image" :alt="product.title" />
-     
+      <h2>{{ product.title }}</h2>
+      <p>\${{ product.price }}</p>
+      <p>{{ product.category }}</p>
+      <div class="rating">
+        <svg v-for="i in 5" :key="i" :class="i <= Math.round(product.rating.rate) ? 'filled' : 'empty'" viewBox="0 0 24 24">
+          <path d="M12 .587l3.668 7.571 8.332 1.151-6.063 5.852 1.428 8.287L12 18.897l-7.365 3.851 1.428-8.287-6.063-5.852 8.332-1.151z"/>
+        </svg>
+      </div>
+      <button class="view-button" @click="viewProduct(product.id)">View Product</button>
     </div>
   </div>
 
@@ -47,3 +55,4 @@ export default {
   }
 };
 </script>
+
